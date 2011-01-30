b16e6e020249ee4e408eba93e75eeb0015583535
Marcello Barnaba <vjt@openssl.it>
2011-01-30 17:30:22 +0100

    Almost finalized the SPs

--- schema.sql
+++ schema.sql
@@ -32,62 +32,71 @@ delimiter GO
   -- Yields a recordset containing the current valid ACLs,
   -- that is, the ones that have no date/time constraints
   -- or those that match with the current date/time.
-  delimiter GO
+  DROP VIEW IF EXISTS current_acls
+  GO
   CREATE VIEW current_acls AS
-  SELECT sources.type AS source_type, source.data AS source,
-         destinations.type AS destination_type, destinations.data AS destination,
-         acl.rewrite_url AS rewrite_url
-  FROM acl
-  INNER JOIN sources      ON sources.id = acl.source_id
-  INNER JOIN destinations ON destinations.id = acl.destination_id
-  WHERE (weekday IS NULL OR (weekday = EXTRACT(weekday FROM GETDATE())) AND
+  SELECT sources.type      AS source_type,
+         sources.data      AS source,
+         destinations.type AS destination_type,
+         destinations.data AS destination,
+         acls.rewrite_url  AS rewrite_url
+  FROM acls
+  INNER JOIN sources      ON sources.id      = acls.source_id
+  INNER JOIN destinations ON destinations.id = acls.destination_id
+  WHERE (weekday IS NULL OR weekday = WEEKDAY(NOW())) AND
         (start_at IS NULL AND end_at IS NULL OR (
-          EXTRACT(time FROM GETDATE()) BETWEEN start_at AND end_at
+          TIME(NOW()) BETWEEN start_at AND end_at
         ))
   GO
 
   -- Checks the current_acls view for one that matches the
   -- given destination type, data, user and host. Returns
   -- the eventual rewrite url if found, NULL otherwise.
-  delimiter GO
+  DROP FUNCTION IF EXISTS sp_check
+  GO
   CREATE FUNCTION sp_check(
     _dest_type varchar(8),
     _dest_data varchar(255),
     _user      varchar(255),
     _host      varchar(15)
   ) RETURNS varchar(255)
-
+  BEGIN
     SELECT rewrite_url FROM current_acls
-    WHERE (destinations_type = _dest_type AND destinations_data = _dest_data) AND
-          ((sources_type = 'user' AND sources_data = _user) OR
-           (sources_type = 'host' AND sources_type = _host))
+    WHERE (destination_type = _dest_type AND destination = _dest_data) AND
+          ((source_type = 'user' AND source = _user) OR
+           (source_type = 'host' AND source = _host))
     INTO @rewrite_url;
 
     RETURN @rewrite_url;
+  END
   GO
 
   -- Checks for an ACL matching the given domain, user
   -- and host using sp_check().
-  delimiter GO
+  DROP FUNCTION IF EXISTS sp_check_domain
+  GO
   CREATE FUNCTION sp_check_domain(
     _domain varchar(255),
     _user   varchar(255),
     _host   varchar(15)
   ) RETURNS varchar(255)
   BEGIN
-    RETURN sp_check('domain', _domain, _user, _host);
+    SELECT sp_check('domain', _domain, _user, _host) INTO @rewrite_url;
+    RETURN @rewrite_url;
   END
   GO
 
   -- Checks for an ACL matching the given URL, user
   -- and host using sp_check().
-  delimiter GO
+  DROP FUNCTION IF EXISTS sp_check_url
+  GO
   CREATE FUNCTION sp_check_url(
     _url    varchar(255),
     _user   varchar(255),
     _host   varchar(15)
-  )
+  ) RETURNS varchar(255)
   BEGIN
-    RETURN sp_check('url', _url, _user, _host);
+    SELECT sp_check('url', _url, _user, _host) INTO @rewrite_url;
+    RETURN @rewrite_url;
   END
   GO
